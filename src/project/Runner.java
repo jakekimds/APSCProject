f8e4a0d50b421d00984771b086e5f52d6d37ba1f
kimy3243 <kimy3243@CA-SU-F106-10.sduhsd.lan>
2018-04-27 14:43:39 -0700

    Stuff

---
+++ src/project/Runner.java
@@ -0,0 +1,28 @@
+package project;
+
+import javax.swing.JFrame;
+import java.awt.Component;
+
+public class Runner extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
+	public Runner()
+	{
+		super("STARFIGHTER");
+		setSize(WIDTH,HEIGHT);
+
+		GraphicsRunner theGame = new GraphicsRunner();
+		((Component)theGame).setFocusable(true);
+
+		getContentPane().add(theGame);
+
+		setVisible(true);
+	}
+
+	public static void main( String args[] )
+	{
+		Runner run = new Runner();
+	}
+}
